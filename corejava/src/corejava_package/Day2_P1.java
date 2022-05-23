f33ce094d2365135404ceea1e0b1419b321e317f
06jyothi <jyothigowda0011@gmail.com>
2022-05-23 14:57:20 +0530

    first commit

---
+++ corejava/src/corejava_package/Day2_P1.java
@@ -0,0 +1,17 @@
+package corejava_package;
+
+public class Day2_P1 {
+	void add(int x,int y)
+	{
+		System.out.println("add 2 number:"+(x+y));
+	}
+	public static void main(String[] args) {
+		//TODO Auto_-generated method stub
+		Day2_P1 ob1=new Day2_P1();
+		ob1.add(5, 7);
+		ob1.add(8,3);
+		
+		
+	}
+
+}
