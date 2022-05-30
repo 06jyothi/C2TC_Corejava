d2f93731a09000ac4e7ece97059627ad6216832b
06jyothi <jyothigowda0011@gmail.com>
2022-05-30 14:56:07 +0530

    first commit

---
+++ corejava/src/corejava_package/SK.java
@@ -0,0 +1,16 @@
+package corejava_package;
+
+public class SK {
+static int age;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SK ob1=new SK();
+SK ob2=new SK();
+ob1.age=40;
+ob2.age=50;
+System.out.println("age is "+ob1.age);
+System.out.println("age is"+ob2.age);
+
+	}
+
+}
