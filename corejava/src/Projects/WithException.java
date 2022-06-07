45ea5179b4fe6a5118155efd41d955fdfb7b4fda
06jyothi <jyothigowda0011@gmail.com>
2022-06-07 15:23:33 +0530

    first commit

---
+++ corejava/src/Projects/WithException.java
@@ -0,0 +1,16 @@
+package Projects;
+
+public class WithException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int d=0,a;
+try {
+	a=42/d;
+	System.out.println("This will not be printed");
+}catch (ArithmeticException e) {
+	System.out.println("Division by zero");
+}
+System.out.println("This will get printed");
+}
+	}
---
+++ corejava/src/Projects/WithoutException.java
@@ -0,0 +1,13 @@
+package Projects;
+
+public class WithoutException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int d=0;
+int a=42/d;
+System.out.println("Will not be printed");
+
+	}
+
+}
